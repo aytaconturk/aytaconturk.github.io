0d2d942a7b965e40adcda1f03dcf287160f44280
AytacONTURK <aytac.onturk@kodpit.com>
2021-04-22 15:12:35 +0300

    project files added

---
+++ inclassactivities/project/assets/js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     // Calculating the distance from the top of the page to the initial position of the navbar, and store it in a variable
+//     var navbarOffest = $(".navbar").offset().top;
+//
+//     $(window).on("scroll", function() {
+//         var navbarHeight = $(".navbar").outerHeight();
+//
+//         if ($(window).scrollTop() >= navbarOffest) {
+//             $(".navbar").addClass("navbar-fixed-top");
+//             $("body").css("padding-top", navbarHeight + "px");
+//         } else {
+//             $(".navbar").removeClass("navbar-fixed-top");
+//             $("body").css("padding-top", "0");
+//         }
+//     });
+// });
