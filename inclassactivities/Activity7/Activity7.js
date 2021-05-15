e825f0131955ab9f3df3be5fa10be604a85374de
AytacONTURK <aytac.onturk@kodpit.com>
2021-05-15 13:10:39 +0300

    Activity7 files added

---
+++ inclassactivities/Activity7/Activity7.js
@@ -0,0 +1,14 @@
+var names = ["Ben", "Joel", "Judy", "Anne"];
+var scores = [88, 98, 77, 88];
+
+var $ = function (id) { return document.getElementById(id); };
+
+
+
+window.onload = function () {
+	$("add").onclick = addScore;
+	$("display_results").onclick = displayResults;
+	$("display_scores").onclick = displayScores;
+};
+
+
