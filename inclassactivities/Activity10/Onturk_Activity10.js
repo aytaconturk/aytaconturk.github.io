7140febff35cc1ea628cf178ce6220c97009c099
AytacONTURK <aytac.onturk@kodpit.com>
2021-05-30 20:13:12 +0300

    files added

---
+++ inclassactivities/Activity10/Onturk_Activity10.js
@@ -0,0 +1,12 @@
+// Document is ready
+$(document).ready(function () {
+
+    $("#birth").datepicker();
+
+    var pr = ["ActionScript", "AppleScript", "Asp", "JavaScript", "Lisp", "Perl", "PHP", "Python"];
+    $("#pr").autocomplete({
+        source: pr
+    });
+
+
+});
